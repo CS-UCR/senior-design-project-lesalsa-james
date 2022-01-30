aa59432366d789316012056c0999525826b29584
sabroops <scarl011@ucr.edu>
2022-01-30 14:57:56 -0800

    begin sideDrawer

--- mern/client/src/components/miscellaneous/SideDrawer.js
+++ mern/client/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import {Box} from "@chakra-ui/layout";
+import {Tooltip} from "@chakra-ui/react";
+import {Button} from "@chakra-ui/button";
+import React, { useState } from 'react';
 
 const SideDrawer = () => {
-  return <div>Side Drawer</div>;
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
+
+  return <>
+  <Box>
+    <Tooltip label="Search users to chat with" hasArrow placement='bottom-end'>
+      <Button variant="ghost">
+        <i class="fas fa-search"></i>
+      </Button>
+    </Tooltip>
+  </Box>
+  </>;
 };
 
 export default SideDrawer;
