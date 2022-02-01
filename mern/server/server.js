c984c65c63f8421391f1b64943e45aaa2f9957c1
sabroops <scarl011@ucr.edu>
2022-02-01 03:55:46 -0800

    Backend for chat

--- mern/server/server.js
+++ mern/server/server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes")
 const chatRoutes = require("./routes/chatRoutes")
+const messageRoutes = require("./routes/messageRoutes")
 const { notFound } = require("./middleware/errorMiddleware")
 
 
@@ -18,8 +19,9 @@ app.get("/", (req,res) => {
     res.send("API is running successfully")
 });
 
-app.use('/api/user', userRoutes)
-app.use('/api/chat', chatRoutes )
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 // app.use(errorHandler);
