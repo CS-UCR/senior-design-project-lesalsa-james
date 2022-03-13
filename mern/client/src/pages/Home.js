ef26c43139e0dcce2c1fcb386ff5d6c7fa571a7a
achen163 <achen163@ucr.edu>
2022-03-13 00:57:51 -0800

    Working on homepage

--- mern/client/src/pages/Home.js
+++ mern/client/src/pages/Home.js
@@ -14,26 +14,11 @@ function Home() {
             <div className='page'>
                 <img src={logo} alt="stuff"/> 
                 <button onClick={() => {history.push('/logIn')}} className='primary'>Sign Up / Login</button>;
-                <h1 className='title'> Find some gamers, Gamer!</h1>
-                <h2 className='game'>Game: </h2>
-                <h2 className='rank'>Rank: </h2>
-                <h2 className='playstyle'>Playstyle: </h2>
-                <h2 className='numplayers'># Players: </h2>
-                <button onClick={() => {history.push('/chat')}} className='chat'>Chat Anonymously</button>
+                <h1 className='title'> Welcome to GamersOnly</h1>
+               
 
-                <button className='val'>Valorant</button>
-                <button className='league'>League</button>
-              
-                <button className='bronze'>Bronze</button>
-                <button className='silver'>Silver</button>
-                <button className='gold'>Gold</button>
-                <button className='plat'>Platinum</button>
-                <button className='diamond'>Diamond</button>
-              
-                <button className='casual'>Casual</button> 
-                <button className='competitive'>Competitive</button> 
+                
 
-                <input className='enter'></input>  
 
             </div>
         );
