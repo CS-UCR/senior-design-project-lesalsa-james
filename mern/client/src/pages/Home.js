cd4077a1bfcb31c105aa1ec99854601d870c2c7e
sabroops <scarl011@ucr.edu>
2022-03-14 04:31:22 -0700

    Landing Page

--- mern/client/src/pages/Home.js
+++ mern/client/src/pages/Home.js
@@ -6,20 +6,24 @@ import logo from "./GamersLogo.png";
 
 
 
-
 function Home() {
         let history = useHistory(); 
 
         return (
-            <div className='page'>
+            <div className='page' id="landing-wrapper">
                 <img src={logo} alt="stuff"/> 
                 <button onClick={() => {history.push('/logIn')}} className='primary'>Sign Up / Login</button>;
-                <h1 className='title'> Welcome to GamersOnly</h1>
+                <header id="showcase">
+                    <h1 className='title'> 
+                        <div className='fade-in-text' > Welcome to GamersOnly</div>
+                    </h1> 
+                </header>
+                <br></br>
+                <br></br>
+                <div id="content" class="container">
+                    If you're looking for fellow lonely gamers, you've come to the right place! Find your dream five stack or duo partner today.
+                </div>
                
-
-                
-
-
             </div>
         );
 }
